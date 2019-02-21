a3cadfdcc857bbc135a8d77344e15f3668c20709
pedicone <jackpedicone@gmail.com>
2019-02-21 15:23:18 -0500

    Slight changes

--- Jack/BinaryDigitalAnalysis.c
+++ Jack/BinaryDigitalAnalysis.c
@@ -1,19 +1,29 @@
+#include <msp430.h>
+#include "BDA.h"
 #include <stdint.h>
-#define MAX_DATA_HISTORY 500
-#define MAX_PULSEWIDTH_HISTORY 5
 
 int BinaryDigitalAnalysis(uint8_t output);
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	P1DIR |= BIT1; //1.1 data output
-	P1DIR &= ~DR; //1.0 data input
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    P1DIR |= BIT1; //1.1 data output
+    P1OUT |= BIT1; //1.1 data outpu
+    P1DIR &= ~DR; //1.0 data input
 
-	while(1){
-	    BinaryDigitalAnalysis(P1IN & ~DR);
-	}
-	return 0;
+    while(1){
+        //BinaryDigitalAnalysis(P1IN & ~DR);
+        if(dyn_window_filt(P1IN & DR))
+        {
+            P1OUT |= BIT1;
+        }
+        else
+        {
+            P1OUT &= ~BIT1;
+        }
+
+    }
+    return 0;
 }
 int BinaryDigitalAnalysis(uint8_t input)
 {
@@ -43,36 +53,36 @@ int BinaryDigitalAnalysis(uint8_t input)
 }
 
 uint8_t dyn_window_filt(uint8_t input){
-    
+
     static uint8_t data_history[MAX_DATA_HISTORY];
     static uint8_t pulse_history[MAX_PULSEWIDTH_HISTORY];
     static uint8_t next_data_index = 0, next_pulse_index = 0, pulse_sum = 0;
     uint8_t i,pulse_width = 0, dyn_sum = 0;
-    
+
     //update history array
     data_history[next_data_index] = input;
-    
+
     //calculate current pulse width
     if(input==1)
         pulse_sum += 1;
-    else{        
+    else{
         pulse_history[next_pulse_index]=pulse_sum;
         pulse_sum = 0;
-        
+
         //increment pulse history index
         if(next_data_index==MAX_PULSEWIDTH_HISTORY-1)
             next_pulse_index += 1;
         else
             next_pulse_index = 0;
     }
-    
-    
+
+
     //find max pulse width out of buffer
     for(i = 0;i<MAX_PULSEWIDTH_HISTORY;i++){
         if(pulse_width<pulse_history[i])
             pulse_width = pulse_history[i];
     }
-    
+
     // sum dynamic window
     if(next_data_index>=pulse_width-1){//dynamic window does not wrap around
         for(i=next_data_index-pulse_width+1;i<next_data_index+1;i++){//sums the dynamic window
@@ -87,14 +97,14 @@ uint8_t dyn_window_filt(uint8_t input){
         }
     }
 
-    
+
     //increment data history index
     if(next_data_index==MAX_DATA_HISTORY-1)
-        next_data += 1;
+        next_data_index += 1;
     else
-        next_data = 0;
-    
-    
+        next_data_index = 0;
+
+
     // return data 1 or 0
     if(dyn_sum>pulse_width/2)
         return 1;
